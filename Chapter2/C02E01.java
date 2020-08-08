d87c766407ee73f1b746db701ec731292e6b46c2
Derpn <beiloquuw@gmail.com>
2020-08-08 14:13:14 +0800

    exercise

--- Chapter2/C02E01.java
+++ Chapter2/C02E01.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Temperature{
+public class C02E01{
 	public static void main(String[] args){
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter a degree in Celsius:");
